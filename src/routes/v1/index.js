286454e8bf6ceb18b706f7fcd96c40d03e0a9366
vivekrajput-93 <viveksinghraajputt@gmail.com>
2023-10-22 14:52:25 +0530

    Added router to complete create Api

--- src/routes/v1/index.js
+++ src/routes/v1/index.js
@@ -5,5 +5,6 @@ const CityController = require("../../controllers/city-Controllers");
 const router = express.Router();
 
 router.post('/city', CityController.create);
+router.delete('/city/:id', CityController.destroy);
 
 module.exports = router
